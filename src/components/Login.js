af9c8e0e3a7184b2b86a59d3a79ce233e702a9d9
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-25 20:49:09 -0500

    Made the styles to the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,18 +12,18 @@ const Login = (props) => {
     }
 
     return(
-        <>
-            <h1>Login</h1>
+        <div className="Login">
+            <h1>LOGIN</h1>
             <form>
                 <div className="form-group">
                     <label>USERNAME</label>
                     <input className="black-box" type="text" value={state.username} name="username" onChange={handleChange}/>
                     <label>PASSWORD</label>
-                    <input className="black-box" type="text" value={state.password} name="password" onChange={handleChange}/>
+                    <input className="black-box" type="password" value={state.password} name="password" onChange={handleChange}/>
                     <button className="black-box" type="submit">Submit</button>
                 </div>
             </form>
-        </>
+        </div>
         
     )
 }
