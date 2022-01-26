d27d3b3b6dc8102d6ddddd8f801411ee9fad81d1
Eduardo Escobar <eduarddo.escobar@gmail.com>
2022-01-25 21:17:38 -0500

    Made the header and styled it

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from './components/Login';
-import FriendsList from './components/FriendsList';
+import FriendList from './components/FriendList';
 import FriendForm from './components/FriendForm';
+import FriendHeader from './components/FriendHeader';
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <FriendHeader />
       <Switch>
-        <Route path="/friendslist" component={FriendsList} />
+        <Route path="/friendlist" component={FriendList} />
         <Route path="/addfriend" component={FriendForm} />
         <Route path="/login" component={Login} />
         <Route path="/" component={Login} />
---
+++ src/components/FriendHeader.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const FriendHeader = (props) => {
+    return(
+        <header>
+            <div className="left">
+                <h2>FRIENDS DATABASE</h2>
+            </div>
+            <div className="right">
+                <Link className="black-box" to="/login">LOGIN</Link>
+                <Link className="black-box" to="/friendlist">FRIENDLIST</Link>
+                <Link className="black-box" to="/addfriend">ADDFRIEND</Link>
+            </div>
+        </header>
+    )
+}
+
+export default FriendHeader;
